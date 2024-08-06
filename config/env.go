82b4bd50f13956ded3ddcec7038a0fec5c785098
Satria Rahmat <sumgmng@gmail.com>
2024-08-06 23:15:58 +0700

    update config

--- config/env.go
+++ config/env.go
@@ -3,6 +3,7 @@ package config
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
@@ -10,6 +11,12 @@ import (
 var (
 	MONGO_URI     string
 	MONGO_DB_NAME string
+
+	SMTP_HOST     string
+	SMTP_PORT     int
+	SMTP_USERNAME string
+	SMTP_PASSWORD string
+	EMAIL_FROM    string
 
 	SECRET_KEY     string
 	SERVER_ADDRESS string
@@ -25,6 +32,15 @@ func LoadConfig() {
 
 	MONGO_URI = os.Getenv("MONGO_URI")
 	MONGO_DB_NAME = os.Getenv("MONGO_DB_NAME")
+
+	SMTP_HOST = os.Getenv("SMTP_HOST")
+	SMTP_PORT, err = strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		log.Fatalf("Invalid SMTP PORT")
+	}
+	SMTP_USERNAME = os.Getenv("SMTP_USERNAME")
+	SMTP_PASSWORD = os.Getenv("SMTP_PASSWORD")
+	EMAIL_FROM = os.Getenv("EMAIL_FROM")
 
 	SECRET_KEY = os.Getenv("SECRET_KEY")
 	SERVER_ADDRESS = os.Getenv("SERVER_ADDRESS")
