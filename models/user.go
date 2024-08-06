7dd7a655c42dde694cd422bb9a9309753ff87afc
Satria Rahmat <sumgmng@gmail.com>
2024-08-06 13:29:50 +0700

    create user model

---
+++ models/user.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID                   primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Username             string             `bson:"username" json:"username" binding:"required,alphanum"`
+	Email                string             `bson:"email" json:"email" binding:"required,email_validation"`
+	Password             string             `bson:"password" json:"password" binding:"required,min=8"`
+	ResetPasswordToken   string             `bson:"reset_password_token,omitempty" json:"reset_password_token,omitempty"`
+	ResetPasswordExpires time.Time          `bson:"reset_password_expires,omitempty" json:"reset_password_expires,omitempty"`
+}
+
+type LoginCredentials struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserTokenResponse struct {
+	Token string `json:"token"`
+}
+
+type UserProfileResponse struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
