4203fd46c795d305ec60882420f98b44317c881c
Satria Rahmat <sumgmng@gmail.com>
2024-08-06 15:57:14 +0700

    create product routes

--- routes/routes.go
+++ routes/routes.go
@@ -15,10 +15,19 @@ func SetupRoutes(router *gin.Engine) {
 	profile := api.Group("/profile")
 	profile.Use(middleware.JWTMiddleware())
 
+	products := api.Group("/products")
+	products.Use(middleware.JWTMiddleware())
 	{
 		auth.POST("/register", controllers.Register)
 		auth.POST("/login", controllers.Login)
+		auth.POST("/forgot_password", controllers.ForgotPassword)
+		auth.POST("/reset_password/:token", controllers.ResetPassword)
 
 		profile.GET("", controllers.Profile)
+
+		products.GET("", controllers.GetProducts)
+		products.POST("", controllers.CreateProduct)
+		products.PUT("/:id", middleware.SlugObjectID("id"), controllers.UpdateProduct)
+		products.DELETE("/:id", middleware.SlugObjectID("id"), controllers.DeleteProduct)
 	}
 }
